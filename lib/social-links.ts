b08fa9a03835a9c1ccb72060d64b17edeff5c174
richardhe789 <richardhe789@gmail.com>
2026-09-08 18:43:07 -0400

    nothing happened (dylan)

--- lib/social-links.ts
+++ lib/social-links.ts
@@ -1,6 +1,24 @@
 export const socialLinks = [
-  { href: "https://www.facebook.com/csa.uva", label: "Facebook Page", icon: "facebook" as const },
-  { href: "https://www.facebook.com/groups/csaatuva", label: "Facebook Group", icon: "facebook" as const },
-  { href: "https://www.instagram.com/csaatuva/", label: "Instagram", icon: "instagram" as const },
-  { href: "https://www.youtube.com/@TheCSAatUVA", label: "YouTube", icon: "youtube" as const },
-] as const
+  {
+    //href: "https://www.facebook.com/csa.uva",
+    href: "https://www.instagram.com/dyuu.lee",
+    label: "Facebook Page",
+    icon: "facebook" as const,
+  },
+  {
+    //href: "https://www.facebook.com/groups/csaatuva",
+    href: "https://www.linkedin.com/in/dylanmlee0303/",
+    label: "Facebook Group",
+    icon: "facebook" as const,
+  },
+  {
+    href: "https://www.instagram.com/csaatuva/",
+    label: "Instagram",
+    icon: "instagram" as const,
+  },
+  {
+    href: "https://www.youtube.com/@TheCSAatUVA",
+    label: "YouTube",
+    icon: "youtube" as const,
+  },
+] as const;
